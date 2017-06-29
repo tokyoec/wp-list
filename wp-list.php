fc8c5039b6cf8bd367dfc778736ea018e817c6ab
root <root@vunus.digilib.net>
2017-06-29 16:49:50 +0900

    add class to get db setting staff

--- wp-list.php
+++ wp-list.php
@@ -26,10 +26,48 @@ function wp_list_add_plugin_admin_menu() {
      );
 }
  
+class SaneDb
+{
+    private $_oDb;
+
+    public function __construct(wpdb $oDb)
+    {
+        $this->_oDb = $oDb;
+    }
+
+    public function __get($sField)
+    {
+        if($sField != '_oDb')
+            return $this->_oDb->$sField;
+    }
+
+    public function __set($sField, $mValue)
+    {
+        if($sField != '_oDb')
+            $this->_oDb->$sField = $mValue;
+    }
+
+    public function __call($sMethod, array $aArgs)
+    {
+        return call_user_func_array(array($this->_oDb, $sMethod), $aArgs);
+    }
+
+    public function getDbName() { return $this->_oDb->dbname;     }
+    public function getDbPass() { return $this->_oDb->dbpassword; }
+    public function getDbHost() { return $this->_oDb->dbhost;     }
+	public function getDbUser() { return $this->_oDb->dbuser; 	}
+}
+
 // 設定画面用のHTML
 function wp_list_display_plugin_admin_page() {
+	global $sanedb;
+	global $wpdb;
+	$sanedb = new SaneDb($wpdb);
+
      echo '<div class="wrap">';
-     echo '<p>Hello_Worldプラグインの管理画面</p>';
+     echo '<p>wp_listプラグインの管理画面</p>';
+     echo $sanedb->getDbName() . "<br>";;
+     echo $sanedb->getDbPass() . "<br>";;
      echo '</div>';
 }
 
