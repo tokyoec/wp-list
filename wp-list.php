5f581bd31a0243998f7ff4bafdf70f2030ffbbe6
chen420 <chen420@gmail.com>
2020-03-04 16:09:23 +0900

    make it work for remote host with mysqli_real_connect

--- wp-list.php
+++ wp-list.php
@@ -8,7 +8,7 @@ Plugin Name: WordPress Site List
 Plugin URI: https://github.com/tokyoec/wp-list
 Description: 管理画面にWordPressサイト一覧表示する。
 Author: Hong Chen
-Version: 1.0
+Version: 1.2
 Author URI: https://github.com/chen420
 */
 
